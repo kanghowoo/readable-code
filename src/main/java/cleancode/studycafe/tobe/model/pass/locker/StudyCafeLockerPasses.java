5788d8225f38c128c63f7d344909e5d50f95d140
kanghowoo <tiger452003@gmail.com>
2025-06-06 11:50:11 +0900

    refactor: move display logic.

--- src/main/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPasses.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPasses.java
@@ -1,7 +1,9 @@
-package cleancode.studycafe.tobe.model;
+package cleancode.studycafe.tobe.model.pass.locker;
 
 import java.util.List;
 import java.util.Optional;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
 
 public class StudyCafeLockerPasses {
     private final List<StudyCafeLockerPass> lockerPasses;
@@ -14,7 +16,7 @@ public class StudyCafeLockerPasses {
         return new StudyCafeLockerPasses(lockerPasses);
     }
 
-    public Optional<StudyCafeLockerPass> findLockerPassBy(StudyCafePass pass) {
+    public Optional<StudyCafeLockerPass> findLockerPassBy(StudyCafeSeatPass pass) {
         return lockerPasses.stream()
                 .filter(pass::isSameDurationType)
                 .findFirst();
